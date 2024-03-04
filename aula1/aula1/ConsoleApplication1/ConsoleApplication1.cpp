011e295b8c2fee9bca18437f52279e59baad46bc
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2024-03-04 13:09:03 -0300

    imshow_imread

--- aula1/aula1/ConsoleApplication1/ConsoleApplication1.cpp
+++ aula1/aula1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
 
 int main() {
-	std::cout << "hello world!" << std::endl;
+	cv::Mat img = cv::imread("C:\\Users\\vinicius\\Desktop\\OpenCoding\\cpp\\Visao_Computacional\\aula1\\images\\scooby.jpg",cv::IMREAD_GRAYSCALE);
+	cv::imshow("Window", img);
+	cv::waitKey(0);
+
+	return 0;
 }
